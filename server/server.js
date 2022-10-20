ba2b9993cc3369f084cba0a632050224b8c7e3d3
Lauren <lburke522@gmail.com>
2022-10-20 18:39:18 -0400

    Implement the Apollo Server and apply it to the Express server as middleware

--- server/server.js
+++ server/server.js
@@ -2,9 +2,20 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+// implement Apollo Server and apply it to the Express server as middleware.
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// implement Apollo Server and apply it to the Express server as middleware.
+const server = new ApolloServer({
+  context: authMiddleware
+});
+
+// implement Apollo Server and apply it to the Express server as middleware.
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,5 +28,8 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
